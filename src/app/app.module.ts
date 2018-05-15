d148d6e8fd7a13c319394b95d86b1ff533c27d24
warrenbrown <warren@katapultmedia.com>
2018-05-15 11:47:21 -0500

    Added propoal form
    adde form to create proposals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -24,7 +25,8 @@ import { ProposalNewComponent } from './proposal/proposal-new/proposal-new.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
