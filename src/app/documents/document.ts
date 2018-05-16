0c3aeaa8f7998b86c1ddbafcc8f2c36f1ffc5e14
warrenbrown <warren@katapultmedia.com>
2018-05-16 07:57:21 -0500

    Added styles and fonts

--- src/app/documents/document.ts
+++ src/app/documents/document.ts
@@ -1,8 +1,11 @@
 export class Document {
-  title: string;
-  description: string;
-  file_url: string;
-  updated_at: string;
-  url: string;
-  image_url: string;
+  constructor (
+    title: string,
+    description: string,
+    file_url: string,
+    updated_at: string,
+    url: string,
+    image_url: string
+  ) { }
+
 }
